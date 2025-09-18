346ce6d57fe31d12230d365574b3b63cf418a663
lnitc <nazebesto@yahoo.com>
2025-09-19 08:52:33 +0900

    Add page metadata, favicon, font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,25 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s  / The Wild Oasis",
+    default: " Welcome / The Wild Oasis",
+    description: "Luxurious cabin hotel, located in the middle of the nature",
+  },
 };
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}
+      >
         <Logo />
         <Navigation />
         <main>{children}</main>
