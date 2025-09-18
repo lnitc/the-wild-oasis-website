05f3608e6f16eaed249cb2fe1299d145181e299a
lnitc <nazebesto@yahoo.com>
2025-09-19 05:39:42 +0900

    Add logic to fetch data in a page

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,7 +1,15 @@
-export default function Page() {
+export default async function Page() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
   return (
     <div>
       <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
