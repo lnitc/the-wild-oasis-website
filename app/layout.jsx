a44f2f2078158f089ff317feac8b97f6c616de69
lnitc <nazebesto@yahoo.com>
2025-09-19 08:20:04 +0900

    Configure tailwind css

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,15 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import "@/app/_styles/globals.css";
+
 export const metadata = {
   title: "The Wild Oasis",
 };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <Logo />
         <Navigation />
         <main>{children}</main>
