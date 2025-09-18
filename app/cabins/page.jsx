63062cd623181b35b7ff1a779cd520391393b901
lnitc <nazebesto@yahoo.com>
2025-09-19 05:59:47 +0900

    Add a client component and an app-wide loader

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,3 +1,4 @@
+import Counter from "../components/Counter";
 export default async function Page() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
@@ -10,6 +11,7 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter />
     </div>
   );
 }
