be42f9e2405b94f8d485f2abb7570fb59d9b01c4
lnitc <nazebesto@yahoo.com>
2025-09-19 09:19:21 +0900

    Improve navigation and root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,7 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
+import Header from "@/app/_components/Header";
+import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
-
-import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: {
@@ -18,12 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}
+        className={`antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col ${josefin.className}`}
       >
-        <Logo />
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
