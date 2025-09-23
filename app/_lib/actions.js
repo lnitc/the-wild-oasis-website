a6be4338b5ed40b956f4b588288334029ae507a2
lnitc <nazebesto@yahoo.com>
2025-09-23 18:09:37 +0900

    Add logic to delete a reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { signIn, signOut, auth } from "@/app/_lib/auth";
 import { supabase } from "@/app/_lib/supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "@/app/_lib/data-service";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -36,4 +37,26 @@ export async function updateGuest(formData) {
   }
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session)
+    throw new Error("You must be logged in to delete your reservation");
+
+  // Check if the booking exists(security measure)
+  const guestBookings = await getBookings(session.user.guestId);
+  const booking = guestBookings.find((b) => b.id === bookingId);
+  if (!booking) throw new Error("Booking not found");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    throw new Error("Booking could not be deleted");
+  }
+
+  revalidatePath("/account/reservations");
 }
