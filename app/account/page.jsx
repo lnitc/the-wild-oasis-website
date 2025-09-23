cbf2021fb73bc2f97177c0c77139cd93a45ae806
lnitc <nazebesto@yahoo.com>
2025-09-23 14:02:08 +0900

    Create new guest on first sign in

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -6,6 +6,7 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
+
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
