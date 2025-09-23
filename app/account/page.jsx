83e8b174c62262f6c075774be7466523ee8cf813
lnitc <nazebesto@yahoo.com>
2025-09-23 13:25:04 +0900

    Build a custom sign in page

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,12 +1,15 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Jonas
+        Welcome, {session.user.name}
       </h2>
     </div>
   );
