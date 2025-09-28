737f2a47d8e0045015ff4e149a449aa11c6c68c5
lnitc <nazebesto@yahoo.com>
2025-09-28 19:32:05 +0900

    Remove stack from error component

--- app/error.jsx
+++ app/error.jsx
@@ -5,7 +5,7 @@ export default function Error({ error, reset }) {
     <main className="flex justify-center items-center flex-col gap-6">
       <h1 className="text-3xl font-semibold">Something went wrong!</h1>
       <p className="text-lg">{error.message}</p>
-      <p>{error.stack}</p>
+      {/* <p>{error.stack}</p> */}
       <button
         onClick={reset}
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
